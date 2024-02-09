3f068d9f70623fd8f167251e30811bc8052ba501
Gayane <ggrigoryan015ait@gmail.com>
2024-02-09 10:51:28 +0400

    Add project files.

---
+++ IMA2, Grigoryan Gayane, Homework 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMA2__Grigoryan_Gayane__Homework_3;
+
+namespace Homework_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryManagementSystem.Run();
+        }
+    }
+
+
+}
+
